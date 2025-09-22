26261e99961966d81ba94224367d8c3b3f3efc68
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-09-22 11:55:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function volume_sphere() {
     //Write your code here
 	event.preventDefault();
-	const userInput=event.target[0].value
+	const volume=document.getElementById("volume");
+	const radius=document.getElementById("radius");
+	const userInput=radius.value;
 	if(userInput>0){
 		const volumeOfSphere=(4/3)*Math.PI*userInput*userInput*userInput;
-		event.target[1].value=volumeOfSphere.toFixed(4);
+		volume.value=volumeOfSphere.toFixed(4);
 		console.log(volumeOfSphere)
 	}else{
-		event.target[1].value="NaN";
+		volume.value="NaN";
 	}
 	
   
