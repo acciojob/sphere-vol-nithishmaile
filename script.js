55586372a1ba85248cdb6a7d45da7d94087584fa
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-09-22 09:19:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function volume_sphere() {
 	event.preventDefault();
 	const userInput=event.target[0].value
 	if(userInput>0){
-		const volumeOfSphere=(4/3)*(3.14)*userInput*userInput*userInput;
-		event.target[1].value=volumeOfSphere.toFixed(volumeOfSphere,4);
+		const volumeOfSphere=(4/3)*Math.PI*userInput*userInput*userInput;
+		event.target[1].value=volumeOfSphere.toFixed(4);
 		console.log(volumeOfSphere)
 	}else{
 		event.target[1].value="NaN";
